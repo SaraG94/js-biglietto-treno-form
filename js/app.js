1c5838f927b643518b8ab1374830f799b62c8081
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-01 17:21:25 +0100

    FEAT js take input.text + select

--- js/app.js
+++ js/app.js
@@ -1,3 +1,10 @@
-console.log('ciao')
 
 const KMPRICE = 0.21 
+
+//prendere i kilometri e l'età
+const inputKwElement = document.querySelector('input#km')
+
+console.log(inputKwElement)
+
+const ageElement = document.getElementById('age')
+console.log(ageElement)
