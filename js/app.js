8e911ee31c54f55b051b59365bc6fced36fcdae1
SaraG94 <sara.gherbassi.94@gmail.com>
2023-02-01 18:26:21 +0100

    FEAT take value kw and age

--- js/app.js
+++ js/app.js
@@ -8,3 +8,22 @@ console.log(inputKwElement)
 
 const ageElement = document.getElementById('age')
 console.log(ageElement)
+
+//prendere bottone
+
+const buttonElement = document.getElementById('pulsante')
+console.log(buttonElement)
+
+//ascoltare bottone
+
+buttonElement.addEventListener('click',function(){
+    console.log('applica modifiche')
+
+    //prendere il valore dei km inseriti dall'utente
+    let kwSelect = inputKwElement.value
+    console.log(kwSelect)
+
+    //prendere il valore dell'età inseriti dall'utente
+    let ageSelect = ageElement.value
+    console.log(ageSelect)
+})
